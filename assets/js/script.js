e8625816595b8ef9fb1361abd38b76211028119a
Kristina Pang <kvypang@gmail.com>
2022-04-07 16:52:41 -0700

    js updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,40 @@
 // current day date stamp 
-var currentDay = moment().format("dddd - MMMM Do, YYYY - h:mm a")
+var currentDay = moment().format("dddd - MMMM Do, YYYY")
 $("#currentDay").text(currentDay)
 
+var currentTime = moment().format("h:mm a")
+$('#currentTime').text(currentTime)
 
-// changing time block colors
-for (let index = 0; index < array.length; index++){
-    const element = array[index];
-    
+
+function saveHandler(){
+    var calText = $(this).siblings("textarea").val()
+    var timeSlot = $(this).parent().attr("id")
+    localStorage.setItem(timeSlot, calText)
 }
 
-// local storage 
-localStorage.getItem("")
+// changing time block colors based on current time - past, current, future
+function timeBlocks() {
+var calTime = $(this).attr("id") 
+console.log(calTime)
+console.log(moment().format("HH"))
+if (calTime > moment().format("HH")) { 
+        $(this).addClass("future")
+} else if(calTime === moment().format("HH")){
+    $(this).addClass("present")
+} else {
+    $(this).addClass("past")
+}}     
+$(".time-block").each(timeBlocks);
+
+//saving entries for each time-block to local storage
+$(".saveBtn").on('click', saveHandler)
+$("#09").children("textarea").val(localStorage.getItem("09"))
+$("#10").children("textarea").val(localStorage.getItem("10"))
+$("#11").children("textarea").val(localStorage.getItem("11"))
+$("#12").children("textarea").val(localStorage.getItem("12"))
+$("#13").children("textarea").val(localStorage.getItem("13"))
+$("#14").children("textarea").val(localStorage.getItem("14"))
+$("#15").children("textarea").val(localStorage.getItem("15"))
+$("#16").children("textarea").val(localStorage.getItem("16"))
+$("#17").children("textarea").val(localStorage.getItem("17"))
+
